27546d751830adc81d1fba08707cff90d9c6ec40
Steve Grice <steve@pagekey.io>
2026-09-24 07:06:16 -0400

    feat: add fastapi, uvicorn to backend

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from server!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def index():
+    return "Hello from server!"
