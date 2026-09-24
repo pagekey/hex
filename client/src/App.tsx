87e53dcafdea8b8a2587263a53f52fec9f8d6c34
Steve Grice <steve@pagekey.io>
2026-09-24 07:19:07 -0400

    fix: put shadcn stuff in src/

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
     <div className="bg-red-400">
       Hello Hex!
+      <Button>Hi</Button>
     </div>
   )
 }
