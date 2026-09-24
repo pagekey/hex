9122e6bb42ec106470d57bc90ed6f850b2a301ee
Steve Grice <steve@pagekey.io>
2026-09-24 06:34:47 -0400

    feat: install shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      // Option 1: Object format
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
 })
